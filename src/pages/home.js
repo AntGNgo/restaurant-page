75acdc449f27443dff615a9eaf0ab9ce206a496c
Anthony Ngo <ango.ext@yieldengineering.net>
2023-05-01 07:48:14 -0700

    organize pages into new foler

---
+++ src/pages/home.js
@@ -0,0 +1,14 @@
+const home = (content) => {
+  const div = document.createElement("div");
+
+  const mainHeader = document
+    .createElement("h1")
+    .appendChild(document.createTextNode("Ant's Breakfast"));
+  const homeCopy = document.createElement("p");
+  const hours = document.createElement("ul");
+
+  div.appendChild(mainHeader);
+  content.appendChild(div);
+};
+
+export { home };
