ca620cded93fff81d26e92917b54f0480e168466
Anthony Ngo <ango.ext@yieldengineering.net>
2023-05-04 08:05:10 -0700

    add copy and image onto home page. style flex.

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,16 +1,31 @@
 import { navigation } from "../navigation";
+import restaurantImg from "../assets/restaurant.jpg";
 
 const home = (content) => {
+  // Load Navigation Module
   navigation(content);
-  const div = document.createElement("div");
 
-  const mainHeader = document
-    .createElement("h1")
-    .appendChild(document.createTextNode("Ant's Breakfast"));
+  // Create Container for home items
+  const div = document.createElement("div");
+  div.classList.add("home-container");
+
+  // Create and populate element for home copy
   const homeCopy = document.createElement("p");
+  homeCopy.textContent =
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+
+  // Create image and add source
+  const homeImg = document.createElement("img");
+  homeImg.src = restaurantImg;
+
+  // Append copy and image to container
+  div.appendChild(homeCopy);
+  div.appendChild(homeImg);
+
+  // Create hours table
   const hours = document.createElement("ul");
 
-  div.appendChild(mainHeader);
+  // Append container to content div
   content.appendChild(div);
 };
 
